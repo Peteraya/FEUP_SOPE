e99b2bd4720609edfaf27d3cac96435b35cab146
Peteraya <up201505460@fe.up.pt>
2017-04-10 18:46:57 +0100

    Add files via upload

--- sfind.c
+++ sfind.c
@@ -94,27 +94,41 @@ int d(void)
 
 void show_dir_content(char * path, char * file)
 {
-    
+ 
+    printf("Path: %s\n", path);
   DIR * d = opendir(path); // open the path
-  if(d==NULL) return; // if was not able return
+  //if(d==NULL) return; // if was not able return
   struct dirent * dir; // for the directory entries
+  
   while ((dir = readdir(d)) != NULL) // if we were able to read somehting from the directory
     {
-        if(dir -> d_type != DT_DIR && dir->d_name == file){
-            printf("%s/%s\n", path,dir->d_name);
-            //printf("%s\n", dir->d_name);
-        }
-        
-        else 
+         printf("OIII\n");
+        // printf("Ficheiros: %s\n",dir->d_name);
+          //printf("File: %s\n",file);
+          //printf("%s/%s\n", path,dir->d_name);
+          
             if(dir -> d_type == DT_DIR && strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0 ) // if it is a directory
       {
         if(fork()==0){
-          printf("%s%s\n",GREEN, dir->d_name); // print its name in green
+          //printf("%s%s\n",GREEN, dir->d_name); // print its name in green
         char d_path[255]; // here I am using sprintf which is safer than strcat
         sprintf(d_path, "%s/%s", path, dir->d_name);
-        show_dir_content(d_path,file); // recall with the new path
+        execlp("show_dir_content","show_dir_content",d_path, file,NULL);
+       // show_dir_content(d_path,file); // recall with the new path
       }
+      
       }
+      
+      else if(dir -> d_type == DT_REG && strcmp(dir->d_name,file)==0){
+            printf("%s/%s\n", path,dir->d_name);
+            return;
+        }
+         else
+             if(dir -> d_type == DT_REG){
+           return;
+        }
+        
+         
       //else
         //  if(dir-> d_type != DT_DIR) // if the type is not directory just print it with blue
         //printf("%s%s\n",BLUE, dir->d_name);
@@ -122,13 +136,44 @@ void show_dir_content(char * path, char * file)
     closedir(d); // finally close the directory
 }
 
+
+void show_dir_conten(char * path,char * file)
+{
+  DIR * d = opendir(path); 
+  if(d==NULL) return; 
+  struct dirent * dir;
+  while ((dir = readdir(d)) != NULL) 
+    {
+      if(dir-> d_type != DT_DIR && strcmp(dir->d_name,file)==0) 
+        printf("%s/%s\n", path, dir->d_name);
+      else
+      if(dir -> d_type == DT_DIR && strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0 ) // if it is a directory
+      {
+          pid_t pid=fork();
+        if(pid==0){
+        char d_path[255]; 
+        sprintf(d_path, "%s/%s", path, dir->d_name);
+         //execlp("show_dir_conten","show_dir_conten",d_path, file,NULL);
+        show_dir_conten(d_path,file); // recall with the new path
+      }
+      }
+    }
+    closedir(d); // finally close the directory
+}
+
 int main(int argc, char **argv)
 {
+     signal(SIGINT, sig_usr);
+ 
+     
 
-  printf("%s\n", NORMAL_COLOR);
-
-    show_dir_content(argv[1], argv[2]);
-
-  printf("%s\n", NORMAL_COLOR);
+//    show_dir_content(argv[1], argv[2]);
+    
+     
+     show_dir_conten(argv[1],argv[2]);
+     
+     
+     
+     
   return(0);
 }
