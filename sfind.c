8176aa60e63f26d5b84d7d1158e59a9de6503164
Peteraya <up201505460@fe.up.pt>
2017-04-13 18:47:53 +0100

    Handler exits with y

--- sfind.c
+++ sfind.c
@@ -15,8 +15,6 @@
 #define MAXCHAR 4096
 
 unsigned long octalValue;
-static pid_t pid_handler; //variavel global
-struct sigaction oldsigaction; //declarar antes de subscribe_SIGINT
 
 
 /*void sigint_handler(int signo) { 
@@ -39,33 +37,45 @@ if(res=='Y' || res=='y'){
 }*/
 
 
+static pid_t pid; //variavel global
+struct sigaction oldsigaction; //declarar antes de subscribe_SIGINT
+
+static pid_t pids[4000];
+
+
+
+
 void sig_handler(int sig){
 	if(sig == SIGINT){
 
 		char res;
+                
 
-	if(getpid()==pid_handler) {
-		write(STDOUT_FILENO, "\nAre you sure you want to terminate (Y/N)?\n", 43); 
-	}
+
+                
+
+	if(getpid()==pids[0]) {
+            //printf("\nPID: %d\n",getpid());
+                write(STDOUT_FILENO, "\nAre you sure you want to terminate (Y/N)? ", 44); 
 		
-	kill(0, SIGTSTP); //envia sinal Stop ao processo pai
+	}
+	
 
 		do{
 			read(STDIN_FILENO, &res, 1);
 			if (res == 'Y' || res == 'y')
 			{
 				kill(0,SIGTERM); //envia sinal de Terminacao ao proceso pai
-			}else
-				if (res == 'N' || res == 'n')
-				{
-					kill(0,SIGCONT); //envia sinal ao processo pai para continuar
-					break;
-				}
+			}
+			else if(res == 'n' || res == 'n')
+                               for(int i=0; i< (sizeof(pids)/sizeof(pids[0]));i++)
+                                            kill(pids[i],SIGCONT);
 		}while(res != 'Y' && res != 'y' && res != 'N' && res != 'n');
 	}
 
 }
 
+//chamar esta função logo no incicio do main
 void subscribe_SIGINT(){
 
     sigaction(SIGTSTP, 0,&oldsigaction);
@@ -84,10 +94,9 @@ void subscribe_SIGINT(){
 
 
 
-void commandExecutionFiles(char* cmd, char* path, char* dir_name, unsigned char dir_type,char* execCommand){
+
+void commandExecutionFiles(char* cmd, char* d_path, unsigned char dir_type,char* execCommand){
     
-    char d_path[255]; 
-        sprintf(d_path, "%s/%s", path, dir_name);
     
             if(strcmp("-print",cmd)==0)
                 printf("%s\n", d_path);
@@ -115,10 +124,28 @@ void commandExecutionFiles(char* cmd, char* path, char* dir_name, unsigned char 
           
 }
 
-void nameFunc(char * executavel,char * path, char * mode, char * file, char * cmd,char * execCommand)
+void nameFunc(char * executavel,char * path, char * mode, char * file, char * cmd,char * execCommand, char* vecPos)
 {
+    
+    
+    int vecPosInt;
+    
+    if(strcmp(vecPos,"XDG_VTNR=7")==0){
+     vecPosInt=0;
+        
+    }
+    else{
+        vecPosInt = atoi(vecPos);
+        vecPosInt++;    
+    }
+    
+    //printf("%d\n",vecPosInt);
+    
      if(cmd==NULL)
-     cmd="-print";  
+     cmd="-print"; 
+     
+     pids[vecPosInt]=getpid();
+     
      
     if(strcmp("d",file)==0 && strcmp("-type",mode)==0) {
            
@@ -146,11 +173,23 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
                     //printf("Perm: %o\n", statPerm);
                     
                     if(statPerm == strtol(file, &ptr, 8) && !(strcmp(path,".") == 0 || strcmp(path,"..") == 0 || (path[0] == '.')))
-                            printf("%s\n", path);
+                    {
+                        
+                    if(strcmp("-print",cmd)==0)
+                printf("%s\n", path);
+
+                if(strcmp("-exec",cmd)==0){
+                if(fork()==0)
+                    execlp(execCommand,execCommand, path,NULL);
+                else
+                    wait(NULL);
+             }
                     
-}
-            
-            
+      }
+                        
+                        
+                    }
+                                
      
   DIR * d = opendir(path); 
   if(d==NULL) return; 
@@ -158,19 +197,21 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
   while ((dir = readdir(d)) != NULL) 
     {
        
+        char d_path[255]; 
+        sprintf(d_path, "%s/%s", path, dir->d_name);
+        
+        
         if(strcmp("-perm", mode) == 0 && dir -> d_type == DT_REG ) {
                     struct stat fileStat;
-                    char dir_path[255]; 
-                    sprintf(dir_path, "%s/%s", path, dir->d_name);
                     
-                    stat(dir_path,&fileStat);
+                    
+                    stat(d_path,&fileStat);
                     char * ptr;
                     int statPerm = fileStat.st_mode&0777;
             
-                    //printf("Perm: %o\n", statPerm);
                     
                     if(statPerm == strtol(file, &ptr, 8) && !(strcmp(dir->d_name,".") == 0 || strcmp(dir->d_name,"..") == 0 || (dir->d_name[0] == '.')))
-                            printf("%s\n", dir_path);
+                            commandExecutionFiles(cmd, d_path,dir->d_type,execCommand);
                     
 }
       
@@ -178,7 +219,7 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
         
       if(strcmp(dir->d_name,file)==0 && strcmp("-name",mode)==0) {
             
-          commandExecutionFiles(cmd, path, dir->d_name,dir->d_type,execCommand);
+          commandExecutionFiles(cmd, d_path,dir->d_type,execCommand);
             return;
       }
       
@@ -187,17 +228,12 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
     
                 
                 if(dir -> d_type == DT_LNK && strcmp("l",file)==0) 
-                    commandExecutionFiles(cmd, path, dir->d_name,dir->d_type,execCommand);
+                    commandExecutionFiles(cmd, d_path,dir->d_type,execCommand);
                     
     
-        if(dir -> d_type == DT_REG && strcmp("f",file)==0){
-            
-            
-            commandExecutionFiles(cmd, path, dir->d_name,dir->d_type,execCommand);
+        if(dir -> d_type == DT_REG && strcmp("f",file)==0)
+            commandExecutionFiles(cmd, d_path,dir->d_type,execCommand);
            
-            }
-            
-            
             }
     
       if(dir -> d_type == DT_DIR && strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0 ) 
@@ -207,9 +243,9 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
            waitpid(pid, NULL, 0);
 
        else{
-        char d_path[255]; 
-        sprintf(d_path, "%s/%s", path, dir->d_name);
-        execlp(executavel,executavel,d_path, mode,file,cmd,execCommand,NULL); 
+        
+        sprintf(vecPos,"%d",vecPosInt);
+        execlp(executavel,executavel,d_path, mode,file,cmd,execCommand,vecPos, NULL); 
       }
       }
       
@@ -222,6 +258,30 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
                     
     
       }
+      
+        if(strcmp("-perm", mode) == 0) {
+                    struct stat fileStat;
+                   
+                    
+                    stat(path,&fileStat);
+                    char * ptr;
+                    int statPerm = fileStat.st_mode&0777;
+            
+                    //printf("Perm: %o\n", statPerm);
+                    
+                    if(statPerm == strtol(file, &ptr, 8) && !(strcmp(path,".") == 0 || strcmp(path,"..") == 0 || (path[0] == '.')))
+                    {
+                        
+                    if(strcmp("-delete",cmd)==0)
+                        execlp("rmdir","rmdir",path,NULL);
+                    
+      }
+                        
+                        
+                    }
+      
+      
+      
     closedir(d); // finally close the directory
 }
 
@@ -272,13 +332,28 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
 
 int main(int argc, char **argv)
 {
-   
-    subscribe_SIGINT();
-	pid_handler=getpid();
-    
-    nameFunc(argv[0],argv[1],argv[2], argv[3], argv[4],argv[5]);
-    
   
+   // signal(SIGINT, sigint_handler);
+
+    
+   /* if (strcmp(argv[2], "-perm") == 0) {
+      char* ptr;
+      octalValue = strtol(argv[3], &ptr, 8); 
+    }*/
+   
+   subscribe_SIGINT();
+    pid=getpid();
+    
+    
+   // printf("Arg 6: %s\n", argv[6]);
+   nameFunc(argv[0],argv[1],argv[2], argv[3], argv[4],argv[5],argv[6]);
+   
+   /* int i =0;
+  while(1){
+      
+   printf("%d\n",i++); 
+   sleep(1);
+  }*/
   exit(0);
 }
 
