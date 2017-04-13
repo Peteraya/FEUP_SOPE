3e2894460072516236fbc84f6cd14c68718891d8
Peteraya <up201505460@fe.up.pt>
2017-04-13 22:10:15 +0100

    Final version without 'n' working in handler

--- sfind.c
+++ sfind.c
@@ -8,39 +8,11 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-#define NORMAL_COLOR  "\x1B[0m"
-#define GREEN  "\x1B[32m"
-#define BLUE  "\x1B[34m"
-
-#define MAXCHAR 4096
-
-unsigned long octalValue;
 
 
-/*void sigint_handler(int signo) { 
+#define MAXL 4000
 
-char res;
-
-if (signo == SIGINT){ 
-
-
-printf("Are you sure you want to terminate (Y/N)?\n");
-scanf("%c",&res);
-
-if(res=='Y' || res=='y'){
-    signal(SIGINT, SIG_DFL);
-    raise(SIGINT);
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-	
-}
-
-}*/
-
-
-static pid_t pid; //variavel global
-struct sigaction oldsigaction; //declarar antes de subscribe_SIGINT
-
-static pid_t pids[4000];
+static pid_t pids[MAXL];
 
 
 
@@ -48,34 +20,37 @@ static pid_t pids[4000];
 void sig_handler(int sig){
 	if(sig == SIGINT){
 
+            
+            	kill(0, SIGTSTP); 
+
 		char res;
-                
-
-
-                
-
-	if(getpid()==pids[0]) {
-            //printf("\nPID: %d\n",getpid());
+		if(getpid()==pids[0]) {
                 write(STDOUT_FILENO, "\nAre you sure you want to terminate (Y/N)? ", 44); 
-		
+               
 	}
 	
-
+	
+                
+                
 		do{
 			read(STDIN_FILENO, &res, 1);
 			if (res == 'Y' || res == 'y')
 			{
 				kill(0,SIGTERM); //envia sinal de Terminacao ao proceso pai
-			}
-			else if(res == 'n' || res == 'n')
-                               for(int i=0; i< (sizeof(pids)/sizeof(pids[0]));i++)
-                                            kill(pids[i],SIGCONT);
+			}else
+				if (res == 'N' || res == 'n')
+				{
+					kill(0,SIGCONT); //envia sinal ao processo pai para continuar
+					break;
+				}
 		}while(res != 'Y' && res != 'y' && res != 'N' && res != 'n');
 	}
 
 }
 
-//chamar esta função logo no incicio do main
+struct sigaction oldsigaction; 
+
+
 void subscribe_SIGINT(){
 
     sigaction(SIGTSTP, 0,&oldsigaction);
@@ -86,10 +61,7 @@ void subscribe_SIGINT(){
 
 	sigaction(SIGTSTP, &newsigaction,0);
 	signal(SIGINT, sig_handler);
-
-	
 }
-
 
 
 
@@ -124,7 +96,7 @@ void commandExecutionFiles(char* cmd, char* d_path, unsigned char dir_type,char*
           
 }
 
-void nameFunc(char * executavel,char * path, char * mode, char * file, char * cmd,char * execCommand, char* vecPos)
+void nameFunc(char * executavel,char * path, char * mode, char * file, char * cmd,char * execCommand,char* vecPos)
 {
     
     
@@ -136,16 +108,14 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
     }
     else{
         vecPosInt = atoi(vecPos);
-        vecPosInt++;    
     }
     
-    //printf("%d\n",vecPosInt);
+    pids[vecPosInt++]=getpid();
+    
+    
     
      if(cmd==NULL)
-     cmd="-print"; 
-     
-     pids[vecPosInt]=getpid();
-     
+     cmd="-print";  
      
     if(strcmp("d",file)==0 && strcmp("-type",mode)==0) {
            
@@ -170,9 +140,9 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
                     char * ptr;
                     int statPerm = fileStat.st_mode&0777;
             
-                    //printf("Perm: %o\n", statPerm);
+                   
                     
-                    if(statPerm == strtol(file, &ptr, 8) && !(strcmp(path,".") == 0 || strcmp(path,"..") == 0 || (path[0] == '.')))
+                    if(statPerm == strtol(file, &ptr, 8))
                     {
                         
                     if(strcmp("-print",cmd)==0)
@@ -209,7 +179,7 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
                     char * ptr;
                     int statPerm = fileStat.st_mode&0777;
             
-                    
+                     //printf("Perm: %o\n", statPerm);
                     if(statPerm == strtol(file, &ptr, 8) && !(strcmp(dir->d_name,".") == 0 || strcmp(dir->d_name,"..") == 0 || (dir->d_name[0] == '.')))
                             commandExecutionFiles(cmd, d_path,dir->d_type,execCommand);
                     
@@ -243,9 +213,8 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
            waitpid(pid, NULL, 0);
 
        else{
-        
-        sprintf(vecPos,"%d",vecPosInt);
-        execlp(executavel,executavel,d_path, mode,file,cmd,execCommand,vecPos, NULL); 
+         sprintf(vecPos,"%d",vecPosInt);
+        execlp(executavel,executavel,d_path, mode,file,cmd,execCommand,vecPos,NULL); 
       }
       }
       
@@ -282,78 +251,22 @@ void nameFunc(char * executavel,char * path, char * mode, char * file, char * cm
       
       
       
-    closedir(d); // finally close the directory
+    closedir(d); 
 }
 
-
-/*void typeFunc(char * executavel, char * path, char * c)
-{
-   
-
-
- if(strcmp(c,"d")==0)
-           printf("%s\n", path);
- 
-  DIR * d = opendir(path); 
-  if(d==NULL) return; 
-  struct dirent * dir;
-  
-  while ((dir = readdir(d)) != NULL) 
-    {
-
-         if(dir -> d_type == DT_LNK && strcmp("l",c)==0) {
-        printf("%s/%s\n", path, dir->d_name);
-      }
-        
-        else if(dir -> d_type == DT_REG && strcmp("f",c)==0) {
-        printf("%s/%s\n", path, dir->d_name);
-      }
-     
-      else if(dir -> d_type == DT_DIR && strcmp(dir->d_name,".")!=0 && strcmp(dir->d_name,"..")!=0 ) 
-      {
-        
-          pid_t pid=fork();
-       if(pid>0)
-           waitpid(pid, NULL, 0);
-       
-       else{
-        char d_path[255]; 
-        sprintf(d_path, "%s/%s", path, dir->d_name);
-        execlp(executavel,executavel,d_path,c, NULL); 
-      }
-      }
-    }
-    
-    
-    closedir(d); // finally close the directory
-}*/
 
 
 
 int main(int argc, char **argv)
 {
   
-   // signal(SIGINT, sigint_handler);
-
-    
-   /* if (strcmp(argv[2], "-perm") == 0) {
-      char* ptr;
-      octalValue = strtol(argv[3], &ptr, 8); 
-    }*/
+   
    
    subscribe_SIGINT();
-    pid=getpid();
     
-    
-   // printf("Arg 6: %s\n", argv[6]);
    nameFunc(argv[0],argv[1],argv[2], argv[3], argv[4],argv[5],argv[6]);
-   
-   /* int i =0;
-  while(1){
-      
-   printf("%d\n",i++); 
-   sleep(1);
-  }*/
+    
+  
   exit(0);
 }
 
